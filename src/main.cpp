8b1fd0f45e706cdc58f3d62c5b49664f2f3e3709
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-06-10 03:00:59 +0300

    feat: update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+int euclid(int a, int b) {
+  while (b != 0) {
+    int t = b;
+    b = a % b;
+    a = t;
+  }
+  return a;
+}
+
 int main() {
-  std::cout << "Hello, world!" << '\n';
+  int a, b;
+
+  std::cout << "To find the greates common denominator between a and b, please "
+               "supply the following:\n";
+  std::cout << "a: ";
+  std::cin >> a;
+  std::cout << "b: ";
+  std::cin >> b;
+  std::cout << euclid(a, b);
 
   return 0;
 }
